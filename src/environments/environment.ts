bc154fa41be5033a25880927b235fc7acf5ecc11
orshalmayev <orsh@ofakim-group.com>
2022-01-30 08:49:34 +0200

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiKey: 'Reui7qqMIm5TAcr6rKHwfsVNitancRs9',
   apiUrls: {
   apiBaseUrl: "http://dataservice.accuweather.com/v1/",
